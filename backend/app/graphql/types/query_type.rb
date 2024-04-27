ec0601861dade90ff7732bc0b88bd7c636cdb238
ponchoay <poncho1122c@gmail.com>
2024-04-27 21:03:46 +0900

    refactor: Resolverを使ってfield毎にファイルを分割

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -28,16 +28,8 @@ module Types
       'Hello World!'
     end
 
-    field :book, Types::BookType, null: false do
-      argument :id, ID, required: true
-    end
-    def book(id:)
-      Book.find(id)
-    end
+    field :book, resolver: Resolvers::BookResolver
 
-    field :books, [Types::BookType], null: false
-    def books
-      Book.all
-    end
+    field :books, resolver: Resolvers::BooksResolver
   end
 end
