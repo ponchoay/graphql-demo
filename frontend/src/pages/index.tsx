294c7784c1c89e97f6a2c2e17c23138950c4b6d6
ponchoay <poncho1122c@gmail.com>
2024-04-28 16:40:11 +0900

    feat: 本のデータを表示

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,27 @@
+import { gql, useQuery } from '@apollo/client'
+
+type Book = {
+  id: number
+  title: string
+}
+
 export default function Home() {
+  const FETCH_BOOKS = gql(`
+    query {
+        books {
+          id
+          title
+        }
+    }
+  `)
+
+  const { data, loading } = useQuery(FETCH_BOOKS)
+
   return (
     <div>
       <h1>GraphQL demo</h1>
+      {loading && <div>読み込み中</div>}
+      {!loading && data.books.map((book: Book) => <p key={book.id}>{book.title}</p>)}
     </div>
   )
 }
