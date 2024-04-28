e133da07ac3e9e66be3b51fbbccbe5fb0f23eb76
ponchoay <poncho1122c@gmail.com>
2024-04-28 16:39:36 +0900

    feat: Apollo Clientを追加

---
+++ frontend/src/apollo/client.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:3010/graphql',
+  cache: new InMemoryCache(),
+  credentials: 'include'
+})
